60db893582a0dad62c8572f94f1780a46d63f0bd
KamenevArtem <112734642+KamenevArtem@users.noreply.github.com>
2023-02-08 02:37:16 +0300

    Improvements

--- correct_diary.py
+++ correct_diary.py
@@ -3,16 +3,22 @@ from datacenter.models import Mark
 from datacenter.models import Chastisement
 from datacenter.models import Lesson
 from datacenter.models import Commendation
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 import random
 
 
+praises = ['Молодец!', 'Отлично!', 'Хорошо!', 'Ты меня приятно удивил!',
+           'Великолепно!', 'Прекрасно!', 'Ты меня очень обрадовал!']
+
 def fix_marks(student_name):
-	student = Schoolkid.objects.filter(full_name__contains=student_name).get()
-	student_bad_marks = Mark.objects.filter(schoolkid=student, points__in=[2,3])
-	for bad_mark_card in student_bad_marks:
-		bad_mark_card.points = 5
-		bad_mark_card.save()
-	print(f"Количество плохих оценок = {student_bad_marks}")
+    try:
+        student = Schoolkid.objects.filter(full_name__contains=student_name).get()
+        changed_marks = Mark.objects.filter(schoolkid=student, points__in=[2,3]).update(points=5)
+        print(f"Было исправлено {changed_marks} оценок")
+    except ObjectDoesNotExist:
+        raise ObjectDoesNotExist("Введенное Вами имя не существует")
+    except MultipleObjectsReturned:
+        raise MultipleObjectsReturned("Введеное имя имеет несколько совпадений")
 
 
 def remove_chastisement(student_name):
@@ -22,24 +28,29 @@ def remove_chastisement(student_name):
 	print("Замечания удалены")
 
 
-def create_recomendation(student_name, subject):
-	student = Schoolkid.objects.filter(full_name__contains=student_name).get()
-	lessons = Lesson.objects.filter(
-                                 subject__title=subject,
-                                 year_of_study=student.year_of_study,
-                                 group_letter=student.group_letter
-                                 ).order_by('-date').get()
-	last_lesson = lessons.first()
-	lesson_subject = last_lesson.subject
-	lesson_teacher = last_lesson.teacher
-	lesson_date = last_lesson.date
-	praises = ['Молодец!', 'Отлично!', 'Хорошо!', 'Ты меня приятно удивил!', 
-            'Великолепно!', 'Прекрасно!', 'Ты меня очень обрадовал!']
-	Commendation.objects.create(
-                             text=random.choice(praises),
-                             created=lesson_date,
-                             schoolkid=student,
-                             subject=lesson_subject,
-                             teacher=lesson_teacher
-                             )
-	print("Рекомендация добавлена")
+def create_recomendation(student_name, subject, praises):
+    try:
+        student = Schoolkid.objects.filter(full_name__contains=student_name).get()
+        lessons = Lesson.objects.filter(
+                                        subject__title=subject,
+                                        year_of_study=student.year_of_study,
+                                        group_letter=student.group_letter
+                                        ).order_by('-date')
+        last_lesson = lessons.first()
+        lesson_subject = last_lesson.subject
+        lesson_teacher = last_lesson.teacher
+        lesson_date = last_lesson.date
+        Commendation.objects.create(
+                                text=random.choice(praises),
+                                created=lesson_date,
+                                schoolkid=student,
+                                subject=lesson_subject,
+                                teacher=lesson_teacher
+                                )
+        print("Рекомендация добавлена")
+    except ObjectDoesNotExist:
+        raise ObjectDoesNotExist("Введенное Вами имя или предмет не найдено")
+    except AttributeError:
+        raise AttributeError("Введеный Вами предмет не найден")
+    except MultipleObjectsReturned:
+        raise MultipleObjectsReturned("Введеное имя имеет несколько совпадений")
